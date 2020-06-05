40f40d6e6211be788ab16e4508d12414277a2232
DeMarcus Kennedy <demarcus@demarcuss-air.lan>
2020-06-05 17:25:44 -0500

    need to add gloria now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
+import menu from "./Images/menu1.JPG";
+import "./App.css";
+import NavBar from "./Components/Nav/NavBar";
 
+import { Button } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <NavBar />
+      <div className="mainContainer">
+        <div className="row contentRow">
+          <div className="col-md-7 menuImageContainer">
+            <Image src={menu} className="menuImage"></Image>
+          </div>
+          <div className="col-md-5">
+            <div className="messageContainer">
+              <h1>The Soul House</h1>
+              <p>
+                This is a message you could say before they press the buy now
+                button. maybe talk about what the resturant is all about and
+                what not. I think this would provide value to the customers. Not
+                only that, it'll add more content to this page
+              </p>
+            </div>
+            {/* <Button variant="warning">Buy Now</Button> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
