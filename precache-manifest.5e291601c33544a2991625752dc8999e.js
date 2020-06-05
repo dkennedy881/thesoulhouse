ac630f8ed383fc80fbfb8e5180282063acb782c3
DeMarcus Kennedy <demarcus@demarcuss-air.lan>
2020-06-04 21:15:58 -0500

    Updates

---
+++ precache-manifest.5e291601c33544a2991625752dc8999e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1649d4409f47207836ccc2a01438f6c0",
+    "url": "/dkennedy881/thesoulhouse/index.html"
+  },
+  {
+    "revision": "b3e16d87e5a76952062e",
+    "url": "/dkennedy881/thesoulhouse/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e6b324325ec8dc1b908",
+    "url": "/dkennedy881/thesoulhouse/static/js/2.cf1347b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dkennedy881/thesoulhouse/static/js/2.cf1347b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e16d87e5a76952062e",
+    "url": "/dkennedy881/thesoulhouse/static/js/main.bfe2ee96.chunk.js"
+  },
+  {
+    "revision": "04a4ccc4af13fd86f24b",
+    "url": "/dkennedy881/thesoulhouse/static/js/runtime-main.6fbb8e92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dkennedy881/thesoulhouse/static/media/logo.5d5d9eef.svg"
+  }
+]);
